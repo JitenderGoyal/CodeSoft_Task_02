b07a854b5374317ad2961ab7f170bd3177e112f8
Jitendar Goyal <110550167+JitenderGoyal@users.noreply.github.com>
2023-11-09 00:20:40 +0530

    Update Task_02.java

--- Task_02.java
+++ Task_02.java
@@ -1,44 +1,44 @@
-// Import the Scanner class from the java.util package to read user input
 import java.util.Scanner;
 
-// Define a class named "Task_02"
 public class Task_02 {
-    // Define the main method, which is the entry point of the program
     public static void main(String[] args) {
-        // Create a Scanner object to read input from the console
         Scanner scanner = new Scanner(System.in);
 
-        // Print a message to the console
         System.out.println("------- Grade Calculator -------");
 
-        // Prompt the user to enter the student's name and store it in the "studentName" variable
         System.out.print("Enter student's name: ");
         String studentName = scanner.nextLine();
 
-        // Prompt the user to enter the number of subjects and store it in the "numSubjects" variable
+        System.out.println();  // Add a one-line space
+
         System.out.print("Enter the number of subjects: ");
         int numSubjects = scanner.nextInt();
+        scanner.nextLine();  // Consume the newline character
 
-        // Initialize a variable "totalMarks" to keep track of the total marks
-        int totalMarks = 0;
+        String[] subjectNames = new String[numSubjects];
 
-        // Use a loop to input marks for each subject
-        for (int i = 1; i <= numSubjects; i++) {
-            // Prompt the user to enter marks for a specific subject and store it in the "marks" variable
-            System.out.print("Enter marks obtained in subject " + i + ": ");
-            int marks = scanner.nextInt();
-
-            // Add the entered marks to the totalMarks
-            totalMarks += marks;
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter the name of subject " + (i + 1) + ": ");
+            subjectNames[i] = scanner.nextLine();  // Read subject names with spaces
         }
 
-        // Calculate the average percentage by dividing the total marks by (numSubjects * 100)
+        System.out.println();  // Add a one-line space
+
+        int totalMarks = 0;
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks obtained in " + subjectNames[i] + ": ");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+            if (i < numSubjects - 1) {
+                scanner.nextLine();  // Consume the newline character
+            }
+        }
+
         double averagePercentage = (double) totalMarks / (numSubjects * 100) * 100;
 
-        // Initialize a variable "grade" to store the student's grade
         String grade;
 
-        // Determine the grade based on the average percentage
         if (averagePercentage >= 90) {
             grade = "A+";
         } else if (averagePercentage >= 80) {
@@ -53,7 +53,6 @@ public class Task_02 {
             grade = "F";
         }
 
-        // Print the results to the console
         System.out.println("------------ Results ------------");
         System.out.println("Student Name: " + studentName);
         System.out.println("Total Marks: " + totalMarks);
@@ -61,7 +60,6 @@ public class Task_02 {
         System.out.println("Grade: " + grade);
         System.out.println("---------------------------------");
 
-        // Close the Scanner to release resources
         scanner.close();
     }
 }
